15290c466e8b1fb35f2e92fac31d6983a0cd826a
CordzB <167384843+CordzB@users.noreply.github.com>
2025-06-10 19:23:52 -0600

    Fix mayusculas/minusculas en Navbar.jsx

--- pagweb-ig/src/App.jsx
+++ pagweb-ig/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 import Footer from './components/Footer'
 import Inicio from './pages/Inicio'
 import Nosotros from './pages/Nosotros'
